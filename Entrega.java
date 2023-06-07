619714b6a6f7a1ca0a5796b1847c9137c2f52d17
Brouse_13 <unaiunai2004@gmail.com>
2023-06-07 18:49:55 +0200

    [UPDATE] Removed todo's

--- Entrega.java
+++ Entrega.java
@@ -773,10 +773,10 @@ class Entrega {
    * Podeu aprofitar el mètode `assertThat` per comprovar fàcilment que un valor sigui `true`.
    */
   public static void main(String[] args) {
-    //Tema1.tests();
-    //Tema2.tests();
+    Tema1.tests();
+    Tema2.tests();
     Tema3.tests();
-    //Tema4.tests();
+    Tema4.tests();
   }
 
   //TODO CAMBIAR ANTES DE LA ENTREGA
